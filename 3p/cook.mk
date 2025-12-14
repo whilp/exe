6509a64bbd0443688ef1d3d75ab1c1e352d90fcd
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-14 19:57:49 +0000

    build: download third-party dependencies to separate output directory

--- 3p/cook.mk
+++ 3p/cook.mk
@@ -1,6 +1,9 @@
 curl = curl -fsSL
 sha256sum = sha256sum
+o = o
+sha256_path = $(CURDIR)/$(dir $@)$(notdir $($(*)_url)).sha256
 
 %/digest:
-	$(curl) -o $(subst /digest,/$(notdir $($(*)_url)),$@) $($(*)_url)
-	cd $(dir $@) && $(sha256sum) -c $(notdir $($(*)_url)).sha256
+	mkdir -p $(o)/$(dir $@)
+	$(curl) -o $(o)/$(subst /digest,/$(notdir $($(*)_url)),$@) $($(*)_url)
+	cd $(o)/$(dir $@) && $(sha256sum) -c $(sha256_path)
