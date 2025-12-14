b0d0569727d0c13cf753de9fafe00f3a57cab60b
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-14 20:03:11 +0000

    build: move sha256 checksums into cook.mk variables

--- 3p/cook.mk
+++ 3p/cook.mk
@@ -1,9 +1,9 @@
 curl = curl -fsSL
 sha256sum = sha256sum
 o = o
-sha256_path = $(CURDIR)/$(dir $@)$(notdir $($(*)_url)).sha256
+pkg = $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$(dir $@))))$(suffix $(notdir $($(*)_url)))
 
 %/digest:
 	mkdir -p $(o)/$(dir $@)
-	$(curl) -o $(o)/$(subst /digest,/$(notdir $($(*)_url)),$@) $($(*)_url)
-	cd $(o)/$(dir $@) && $(sha256sum) -c $(sha256_path)
+	$(curl) -o $(o)/$(subst /digest,/$(pkg),$@) $($(*)_url)
+	cd $(o)/$(dir $@) && echo "$($(*)_sha256)  $(pkg)" | $(sha256sum) -c
--- 3p/cosmocc/cook.mk
+++ 3p/cosmocc/cook.mk
@@ -1,1 +1,2 @@
 3p/cosmocc_url = https://github.com/jart/cosmopolitan/releases/download/4.0.2/cosmocc-4.0.2.zip
+3p/cosmocc_sha256 = 85b8c37a406d862e656ad4ec14be9f6ce474c1b436b9615e91a55208aced3f44
