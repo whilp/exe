18bb775074e29f63ea5ecceb5823864d2cea1543
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-14 20:12:07 +0000

    build: split fetch/digest/extract into separate steps

--- 3p/cook.mk
+++ 3p/cook.mk
@@ -1,9 +1,18 @@
 curl = curl -fsSL
 sha256sum = sha256sum
+unzip = unzip -q
 o = o
 pkg = $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$(dir $@))))$(suffix $(notdir $($(*)_url)))
 
-%/digest:
+$(o)/%: %/digest
+	cd $(o)/$* && $(unzip) -o $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$*)))$(suffix $(notdir $($*_url)))
+	touch $@
+
+%/digest: %/fetch
+	cd $(o)/$(dir $<) && echo "$($(*)_sha256)  $(pkg)" | $(sha256sum) -c
+	touch $@
+
+%/fetch:
 	mkdir -p $(o)/$(dir $@)
-	$(curl) -o $(o)/$(subst /digest,/$(pkg),$@) $($(*)_url)
-	cd $(o)/$(dir $@) && echo "$($(*)_sha256)  $(pkg)" | $(sha256sum) -c
+	$(curl) -o $(o)/$(subst /fetch,/$(pkg),$@) $($(*)_url)
+	touch $@
