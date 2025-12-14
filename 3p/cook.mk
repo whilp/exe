e036d688c2b9db2370ac111e0a86d9429ea2a5be
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-14 20:16:41 +0000

    build: use cosmos binaries with fallback to system tools

--- 3p/cook.mk
+++ 3p/cook.mk
@@ -1,18 +1,16 @@
-curl = curl -fsSL
-sha256sum = sha256sum
-unzip = unzip -q
 o = o
+cosmos_bin = $(CURDIR)/$(o)/3p/cosmos/bin
 pkg = $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$(dir $@))))$(suffix $(notdir $($(*)_url)))
 
 $(o)/%: %/digest
-	cd $(o)/$* && $(unzip) -o $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$*)))$(suffix $(notdir $($*_url)))
+	cd $(o)/$* && $(if $(findstring 3p/cosmos,$*),unzip,$(if $(wildcard $(cosmos_bin)/unzip),$(cosmos_bin)/unzip,unzip)) -q -o $(subst /,.,$(patsubst %/,%,$(subst 3p/,,$*)))$(suffix $(notdir $($*_url)))
 	touch $@
 
 %/digest: %/fetch
-	cd $(o)/$(dir $<) && echo "$($(*)_sha256)  $(pkg)" | $(sha256sum) -c
+	cd $(o)/$(dir $<) && echo "$($(*)_sha256)  $(pkg)" | $(if $(findstring 3p/cosmos,$@),sha256sum,$(if $(wildcard $(cosmos_bin)/sha256sum),$(cosmos_bin)/sha256sum,sha256sum)) -c
 	touch $@
 
 %/fetch:
 	mkdir -p $(o)/$(dir $@)
-	$(curl) -o $(o)/$(subst /fetch,/$(pkg),$@) $($(*)_url)
+	$(if $(findstring 3p/cosmos,$@),curl,$(if $(wildcard $(cosmos_bin)/curl),$(cosmos_bin)/curl,curl)) -fsSL -o $(o)/$(subst /fetch,/$(pkg),$@) $($(*)_url)
 	touch $@
