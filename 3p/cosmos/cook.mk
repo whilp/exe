b1374374eaa9ef60699befa461df516f589a13e9
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-14 20:05:17 +0000

    cosmos: add cosmos third-party dependency

---
+++ 3p/cosmos/cook.mk
@@ -0,0 +1,2 @@
+3p/cosmos_url = https://github.com/jart/cosmopolitan/releases/download/4.0.2/cosmos-4.0.2.zip
+3p/cosmos_sha256 = 494ecbd87c2f2f622f91066d4fe5d9ffc1aaaa13de02db1714dd84d014ed398f
--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 include 3p/cook.mk
 include 3p/cosmocc/cook.mk
+include 3p/cosmos/cook.mk
