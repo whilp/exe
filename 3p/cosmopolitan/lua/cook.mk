5059398da671c11614c6728bada4c2e56e201d61
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 02:01:26 +0000

    lua: reorganize files under 3p/cosmopolitan/lua

--- 3p/cosmopolitan/lua/cook.mk
+++ 3p/cosmopolitan/lua/cook.mk
@@ -1,15 +1,16 @@
 lua_bin := o/3p/lua/lua
-$(lua_bin): o/3p/cosmopolitan 3p/lua/linit.c.patch 3p/lua/BUILD.mk.patch
+$(lua_bin): o/3p/cosmopolitan 3p/cosmopolitan/lua/linit.c.patch 3p/cosmopolitan/lua/BUILD.mk.patch
 	mkdir -p o/3p/lua
-	cd $(cosmopolitan_src)/third_party/lua && patch -p0 -N < $(CURDIR)/3p/lua/linit.c.patch || true
+	cd $(cosmopolitan_src)/third_party/lua && patch -p0 -N < $(CURDIR)/3p/cosmopolitan/lua/linit.c.patch || true
 	# currently included modules from tool/net/
 	cp $(cosmopolitan_src)/tool/net/lre.c $(cosmopolitan_src)/third_party/lua/lre.c
 	cp $(cosmopolitan_src)/tool/net/lpath.c $(cosmopolitan_src)/third_party/lua/lpath.c
 	cp $(cosmopolitan_src)/tool/net/largon2.c $(cosmopolitan_src)/third_party/lua/largon2.c
 	cp $(cosmopolitan_src)/tool/net/lsqlite3.c $(cosmopolitan_src)/third_party/lua/lsqlite3.c
 	# note: lunix.c already built separately via o/3p/cosmopolitan/lunix.a
-	cp 3p/lua/BUILD.mk.patch $(cosmopolitan_src)/third_party/lua/BUILD.mk
+	cd $(cosmopolitan_src)/third_party/lua && patch -p0 -N < $(CURDIR)/3p/cosmopolitan/lua/BUILD.mk.patch || true
 	cd $(cosmopolitan_src) && rm -f o//third_party/lua/lua.a.pkg o//third_party/lua/lua.pkg o//third_party/lua/lua
+	cd $(cosmopolitan_src) && $(make) -j8 o//third_party/argon2/argon2.a.pkg o//third_party/sqlite3/sqlite3.a.pkg
 	cd $(cosmopolitan_src) && $(make) -j8 o//third_party/lua/lua
 	cp $(cosmopolitan_src)/o//third_party/lua/lua o/3p/lua/lua
 
@@ -22,12 +23,12 @@ o/3p/lua: o/3p/lua/lua o/3p/cosmopolitan/luac o/3p/cosmopolitan/lunix.a o/3p/lua
 	cd o/3p/lua && zip -q bin/lua .lua/luaunit.lua
 	touch $@
 
-3p/lua/test: $(lua_bin)
-	$(lua) 3p/lua/test_unix.lua
-	$(lua) 3p/lua/test_modules.lua
-	$(lua) 3p/lua/test_re.lua
-	$(lua) 3p/lua/test_path.lua
-	$(lua) 3p/lua/test_argon2.lua
-	$(lua) 3p/lua/test_sqlite.lua
+3p/cosmopolitan/lua/test: $(lua_bin)
+	$(lua) 3p/cosmopolitan/lua/test_unix.lua
+	$(lua) 3p/cosmopolitan/lua/test_modules.lua
+	$(lua) 3p/cosmopolitan/lua/test_re.lua
+	$(lua) 3p/cosmopolitan/lua/test_path.lua
+	$(lua) 3p/cosmopolitan/lua/test_argon2.lua
+	$(lua) 3p/cosmopolitan/lua/test_sqlite.lua
 
-.PHONY: 3p/lua/test
+.PHONY: 3p/cosmopolitan/lua/test
