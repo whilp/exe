b03b3320b3c85c1e64021805fdf505ef3a58e0a1
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 03:36:11 +0000

    cosmos: add build rules for downloading and extracting

--- 3p/cosmos/cook.mk
+++ 3p/cosmos/cook.mk
@@ -1,2 +1,15 @@
-3p/cosmos_url = https://github.com/jart/cosmopolitan/releases/download/4.0.2/cosmos-4.0.2.zip
-3p/cosmos_sha256 = 494ecbd87c2f2f622f91066d4fe5d9ffc1aaaa13de02db1714dd84d014ed398f
+cosmos_url := https://github.com/jart/cosmopolitan/releases/download/4.0.2/cosmos-4.0.2.zip
+cosmos_sha256 := 494ecbd87c2f2f622f91066d4fe5d9ffc1aaaa13de02db1714dd84d014ed398f
+cosmos_dir := $(3p)/cosmos
+cosmos_zip := $(cosmos_dir)/cosmos.zip
+cosmos_bin := $(cosmos_dir)/bin
+
+$(cosmos_bin): $(cosmos_zip)
+	$(unzip) -q -o $< -d $(cosmos_dir)
+
+$(cosmos_zip): $(cosmos_dir)
+	$(curl) -fsSL -o $@ $(cosmos_url)
+	cd $(dir $@) && echo "$(cosmos_sha256)  $(notdir $@)" | $(sha256sum) -c
+
+$(cosmos_dir):
+	mkdir -p $@
