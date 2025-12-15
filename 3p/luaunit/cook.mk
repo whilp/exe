77fe7e3862fbb5dff0004d18c2fb760fa8e93768
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 03:56:39 +0000

    luaunit: follow cosmocc/cosmos pattern for downloading

--- 3p/luaunit/cook.mk
+++ 3p/luaunit/cook.mk
@@ -1,10 +1,11 @@
-luaunit_url = https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_4/luaunit.lua
-luaunit_sha256 = bf3e3fb25b77739fa1ebc324582776d26486e32e49c150628bc21b9b9e6ce645
+luaunit_url := https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_4/luaunit.lua
+luaunit_sha256 := bf3e3fb25b77739fa1ebc324582776d26486e32e49c150628bc21b9b9e6ce645
+luaunit_dir := $(3p)/luaunit
+luaunit_file := $(luaunit_dir)/luaunit.lua
 
-o/3p/luaunit/luaunit.lua:
-	mkdir -p o/3p/luaunit
-	$(curl) -fsSL -o $@ $(luaunit_url)
-	cd o/3p/luaunit && echo "$(luaunit_sha256)  luaunit.lua" | $(sha256sum) -c
+$(luaunit_file): $(luaunit_dir)
+	$(curl) -o $@ $(luaunit_url)
+	cd $(dir $@) && echo "$(luaunit_sha256)  $(notdir $@)" | $(sha256sum) -c
 
-o/3p/luaunit: o/3p/luaunit/luaunit.lua
-	touch $@
+$(luaunit_dir):
+	mkdir -p $@
