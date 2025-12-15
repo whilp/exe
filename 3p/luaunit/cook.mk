3baffe121c3541d77b22ef3fea74f4ab41440aed
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 04:25:05 +0000

    luaunit: produce .lua directory for bundling

--- 3p/luaunit/cook.mk
+++ 3p/luaunit/cook.mk
@@ -2,6 +2,7 @@ luaunit_url := https://raw.githubusercontent.com/bluebird75/luaunit/LUAUNIT_V3_4
 luaunit_sha256 := bf3e3fb25b77739fa1ebc324582776d26486e32e49c150628bc21b9b9e6ce645
 luaunit_dir := $(3p)/luaunit
 luaunit_file := $(luaunit_dir)/luaunit.lua
+luaunit_lua := $(luaunit_dir)/.lua/luaunit.lua
 
 $(luaunit_file): $(luaunit_dir)
 	$(curl) -o $@ $(luaunit_url)
@@ -9,3 +10,7 @@ $(luaunit_file): $(luaunit_dir)
 
 $(luaunit_dir):
 	mkdir -p $@
+
+$(luaunit_lua): $(luaunit_file)
+	mkdir -p $(dir $@)
+	cp $(luaunit_file) $@
