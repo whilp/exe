4e3e2d5ca91218d0be274f9e580d58b0b17a6a15
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 03:50:38 +0000

    cosmopolitan: build lua from cosmopolitan source using cosmocc

--- 3p/cosmopolitan/cook.mk
+++ 3p/cosmopolitan/cook.mk
@@ -1,17 +1,22 @@
 3p/cosmopolitan_url = https://github.com/jart/cosmopolitan/releases/download/4.0.2/cosmopolitan-4.0.2.tar.gz
 3p/cosmopolitan_sha256 = e466106b18064e0c996ef64d261133af867bccd921ad14e54975d89aa17a8717
-cosmopolitan_src := $(o)/3p/cosmopolitan/cosmopolitan-4.0.2
+cosmopolitan_dir := $(3p)/cosmopolitan
+cosmopolitan_tarball := $(cosmopolitan_dir)/cosmopolitan.tar.gz
+cosmopolitan_src := $(cosmopolitan_dir)/cosmopolitan-4.0.2
 
-o/3p/cosmopolitan/%: o/3p/cosmopolitan o/3p/cosmocc o/3p/cosmos
-	cd $(cosmopolitan_src) && $(make) -j8 o//$(*)
+$(cosmopolitan_src): $(cosmopolitan_tarball)
+	cd $(cosmopolitan_dir) && $(tar) -xzf $(notdir $<)
+
+$(cosmopolitan_tarball): $(cosmopolitan_dir)
+	$(curl) -o $@ $(3p/cosmopolitan_url)
+	cd $(dir $@) && echo "$(3p/cosmopolitan_sha256)  $(notdir $@)" | $(sha256sum) -c
+
+$(cosmopolitan_dir):
+	mkdir -p $@
+
+cosmopolitan_lua := $(cosmopolitan_dir)/lua
+
+$(cosmopolitan_lua): $(cosmopolitan_src) $(cosmocc_bin) $(cosmos_bin)
+	cd $(cosmopolitan_src) && $(make) -j8 o//third_party/lua/lua
 	mkdir -p $(dir $@)
-	cp $(cosmopolitan_src)/o//$(*) $@
-
-o/3p/cosmopolitan/lua: o/3p/cosmopolitan/third_party/lua/lua
-	cp o/3p/cosmopolitan/third_party/lua/lua o/3p/cosmopolitan/lua
-
-o/3p/cosmopolitan/luac: o/3p/cosmopolitan/third_party/lua/luac
-	cp o/3p/cosmopolitan/third_party/lua/luac o/3p/cosmopolitan/luac
-
-o/3p/cosmopolitan/lunix.a: o/3p/cosmopolitan/third_party/lua/lunix.a
-	cp o/3p/cosmopolitan/third_party/lua/lunix.a o/3p/cosmopolitan/lunix.a
+	cp $(cosmopolitan_src)/o//third_party/lua/lua $@
