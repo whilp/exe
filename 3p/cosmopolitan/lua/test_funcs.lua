d0d80ce012c4292a985012b8819d5253bb4ccaab
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 05:58:22 +0000

    cosmopolitan: add comprehensive module and function tests
    Consolidate tests into test_modules.lua and test_funcs.lua to check
    availability of redbean modules (unix, path, re, argon2, lsqlite3) and
    generally applicable functions (Encode*, Decode*, Escape*, Hash*, etc).
    Currently only unix module is available.

---
+++ 3p/cosmopolitan/lua/test_funcs.lua
@@ -0,0 +1,187 @@
+function test_EncodeBase64_exists()
+    lu.assertNotNil(EncodeBase64, "EncodeBase64 should be available")
+end
+
+function test_DecodeBase64_exists()
+    lu.assertNotNil(DecodeBase64, "DecodeBase64 should be available")
+end
+
+function test_EncodeBase32_exists()
+    lu.assertNotNil(EncodeBase32, "EncodeBase32 should be available")
+end
+
+function test_DecodeBase32_exists()
+    lu.assertNotNil(DecodeBase32, "DecodeBase32 should be available")
+end
+
+function test_EncodeHex_exists()
+    lu.assertNotNil(EncodeHex, "EncodeHex should be available")
+end
+
+function test_DecodeHex_exists()
+    lu.assertNotNil(DecodeHex, "DecodeHex should be available")
+end
+
+function test_EncodeLatin1_exists()
+    lu.assertNotNil(EncodeLatin1, "EncodeLatin1 should be available")
+end
+
+function test_DecodeLatin1_exists()
+    lu.assertNotNil(DecodeLatin1, "DecodeLatin1 should be available")
+end
+
+function test_EncodeJson_exists()
+    lu.assertNotNil(EncodeJson, "EncodeJson should be available")
+end
+
+function test_DecodeJson_exists()
+    lu.assertNotNil(DecodeJson, "DecodeJson should be available")
+end
+
+function test_EncodeLua_exists()
+    lu.assertNotNil(EncodeLua, "EncodeLua should be available")
+end
+
+function test_EscapeHtml_exists()
+    lu.assertNotNil(EscapeHtml, "EscapeHtml should be available")
+end
+
+function test_EscapePath_exists()
+    lu.assertNotNil(EscapePath, "EscapePath should be available")
+end
+
+function test_EscapeSegment_exists()
+    lu.assertNotNil(EscapeSegment, "EscapeSegment should be available")
+end
+
+function test_EscapeParam_exists()
+    lu.assertNotNil(EscapeParam, "EscapeParam should be available")
+end
+
+function test_EscapeLiteral_exists()
+    lu.assertNotNil(EscapeLiteral, "EscapeLiteral should be available")
+end
+
+function test_EscapeFragment_exists()
+    lu.assertNotNil(EscapeFragment, "EscapeFragment should be available")
+end
+
+function test_EscapeUser_exists()
+    lu.assertNotNil(EscapeUser, "EscapeUser should be available")
+end
+
+function test_EscapePass_exists()
+    lu.assertNotNil(EscapePass, "EscapePass should be available")
+end
+
+function test_EscapeHost_exists()
+    lu.assertNotNil(EscapeHost, "EscapeHost should be available")
+end
+
+function test_Md5_exists()
+    lu.assertNotNil(Md5, "Md5 should be available")
+end
+
+function test_Sha1_exists()
+    lu.assertNotNil(Sha1, "Sha1 should be available")
+end
+
+function test_Sha224_exists()
+    lu.assertNotNil(Sha224, "Sha224 should be available")
+end
+
+function test_Sha256_exists()
+    lu.assertNotNil(Sha256, "Sha256 should be available")
+end
+
+function test_Sha384_exists()
+    lu.assertNotNil(Sha384, "Sha384 should be available")
+end
+
+function test_Sha512_exists()
+    lu.assertNotNil(Sha512, "Sha512 should be available")
+end
+
+function test_GetCryptoHash_exists()
+    lu.assertNotNil(GetCryptoHash, "GetCryptoHash should be available")
+end
+
+function test_Crc32_exists()
+    lu.assertNotNil(Crc32, "Crc32 should be available")
+end
+
+function test_Crc32c_exists()
+    lu.assertNotNil(Crc32c, "Crc32c should be available")
+end
+
+function test_Deflate_exists()
+    lu.assertNotNil(Deflate, "Deflate should be available")
+end
+
+function test_Inflate_exists()
+    lu.assertNotNil(Inflate, "Inflate should be available")
+end
+
+function test_Decimate_exists()
+    lu.assertNotNil(Decimate, "Decimate should be available")
+end
+
+function test_ParseUrl_exists()
+    lu.assertNotNil(ParseUrl, "ParseUrl should be available")
+end
+
+function test_EncodeUrl_exists()
+    lu.assertNotNil(EncodeUrl, "EncodeUrl should be available")
+end
+
+function test_ParseIp_exists()
+    lu.assertNotNil(ParseIp, "ParseIp should be available")
+end
+
+function test_FormatIp_exists()
+    lu.assertNotNil(FormatIp, "FormatIp should be available")
+end
+
+function test_ResolveIp_exists()
+    lu.assertNotNil(ResolveIp, "ResolveIp should be available")
+end
+
+function test_Curve25519_exists()
+    lu.assertNotNil(Curve25519, "Curve25519 should be available")
+end
+
+function test_GetRandomBytes_exists()
+    lu.assertNotNil(GetRandomBytes, "GetRandomBytes should be available")
+end
+
+function test_Bingbong_exists()
+    lu.assertNotNil(Bingbong, "Bingbong should be available")
+end
+
+function test_UuidV4_exists()
+    lu.assertNotNil(UuidV4, "UuidV4 should be available")
+end
+
+function test_UuidV7_exists()
+    lu.assertNotNil(UuidV7, "UuidV7 should be available")
+end
+
+function test_FormatHttpDateTime_exists()
+    lu.assertNotNil(FormatHttpDateTime, "FormatHttpDateTime should be available")
+end
+
+function test_ParseHttpDateTime_exists()
+    lu.assertNotNil(ParseHttpDateTime, "ParseHttpDateTime should be available")
+end
+
+function test_Fetch_exists()
+    lu.assertNotNil(Fetch, "Fetch should be available")
+end
+
+function test_MeasureEntropy_exists()
+    lu.assertNotNil(MeasureEntropy, "MeasureEntropy should be available")
+end
+
+function test_VisualizeControlCodes_exists()
+    lu.assertNotNil(VisualizeControlCodes, "VisualizeControlCodes should be available")
+end
