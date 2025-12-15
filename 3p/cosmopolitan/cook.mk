b326d8cfa788dd36700545005585cf428e6d0930
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 04:49:10 +0000

    cosmopolitan: move lua build into lua subdirectory
    Reorganize cosmopolitan lua-related build logic into 3p/cosmopolitan/lua/
    subdirectory to better separate concerns and prepare for additional
    lua tooling.

--- 3p/cosmopolitan/cook.mk
+++ 3p/cosmopolitan/cook.mk
@@ -13,12 +13,3 @@ $(cosmopolitan_tarball): $(cosmopolitan_dir)
 
 $(cosmopolitan_dir):
 	mkdir -p $@
-
-cosmopolitan_lua := $(cosmopolitan_dir)/lua
-cosmopolitan_lua_libs := $(luaunit_zip)
-
-$(cosmopolitan_lua): $(cosmopolitan_src) $(cosmocc_bin) $(cosmos_bin) $(cosmopolitan_lua_libs)
-	cd $(cosmopolitan_src) && $(make) -j8 o//third_party/lua/lua
-	mkdir -p $(dir $@)
-	cp $(cosmopolitan_src)/o//third_party/lua/lua $@
-	$(foreach lib,$(cosmopolitan_lua_libs),cd $(dir $@) && $(unzip) -o $(lib);)
--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 include 3p/cook.mk
 include 3p/luaunit/cook.mk
 include 3p/cosmopolitan/cook.mk
+include 3p/cosmopolitan/lua/cook.mk
 
 #test: o/3p/lua
 #	3p/cosmocc/test
