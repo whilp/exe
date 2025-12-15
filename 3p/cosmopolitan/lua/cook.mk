e62be5b31645067e7c6993a6ab4ef30eff93ebb6
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-15 05:27:02 +0000

    cosmopolitan: extract lua deps into variable

--- 3p/cosmopolitan/lua/cook.mk
+++ 3p/cosmopolitan/lua/cook.mk
@@ -1,7 +1,8 @@
 cosmopolitan_lua := $(cosmopolitan_dir)/lua/lua
 cosmopolitan_lua_libs := $(luaunit_libs)
+cosmopolitan_lua_deps := $(cosmopolitan_src) $(cosmocc_bin) $(cosmos_bin) $(cosmopolitan_lua_libs)
 
-$(cosmopolitan_lua): $(cosmopolitan_src) $(cosmocc_bin) $(cosmos_bin) $(cosmopolitan_lua_libs)
+$(cosmopolitan_lua): $(cosmopolitan_lua_deps)
 	cd $(cosmopolitan_src) && $(make) -j8 o//third_party/lua/lua
 	mkdir -p $(dir $@)
 	cp $(cosmopolitan_src)/o//third_party/lua/lua $@
