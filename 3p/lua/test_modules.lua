377da3680d404d9dfc56201091f7f5ab5811bdbe
Will Maier <189851+whilp@users.noreply.github.com>
2025-12-19 07:13:26 +0000

    lua: consolidate modules under cosmo namespace
    All modules now accessible as cosmo submodules:
    - cosmo.unix
    - cosmo.path
    - cosmo.re
    - cosmo.argon2
    - cosmo.sqlite3
    
    Usage: local cosmo = require('cosmo'); cosmo.unix.getpid()

--- 3p/lua/test_modules.lua
+++ 3p/lua/test_modules.lua
@@ -1,53 +1,60 @@
--- test modules that require explicit require()
-local path = require('path')
-local re = require('re')
-local argon2 = require('argon2')
-local lsqlite3 = require('lsqlite3')
+-- test cosmo submodules
+local cosmo = require('cosmo')
 
-function test_unix_module_exists()
-    lu.assertNotNil(unix, "unix module should be compiled into lua")
+function test_cosmo_module_exists()
+    lu.assertNotNil(cosmo, "cosmo module should be compiled into lua")
 end
 
-function test_path_module_exists()
-    lu.assertNotNil(path, "path module should be compiled into lua")
+function test_cosmo_unix_exists()
+    lu.assertNotNil(cosmo.unix, "cosmo.unix should exist")
 end
 
-function test_path_basename()
-    lu.assertEquals(path.basename("/foo/bar/baz.txt"), "baz.txt")
-    lu.assertEquals(path.basename("/foo/bar/"), "bar")
-    lu.assertEquals(path.basename("file.txt"), "file.txt")
+function test_cosmo_unix_getpid()
+    lu.assertNotNil(cosmo.unix.getpid)
+    local pid = cosmo.unix.getpid()
+    lu.assertTrue(pid > 0)
 end
 
-function test_path_dirname()
-    lu.assertEquals(path.dirname("/foo/bar/baz.txt"), "/foo/bar")
-    lu.assertEquals(path.dirname("/foo/bar/"), "/foo")
+function test_cosmo_path_exists()
+    lu.assertNotNil(cosmo.path, "cosmo.path should exist")
 end
 
-function test_path_join()
-    lu.assertEquals(path.join("foo", "bar"), "foo/bar")
-    lu.assertEquals(path.join("/foo", "bar"), "/foo/bar")
-    lu.assertEquals(path.join("foo", "/bar"), "/bar")
+function test_cosmo_path_basename()
+    lu.assertEquals(cosmo.path.basename("/foo/bar/baz.txt"), "baz.txt")
+    lu.assertEquals(cosmo.path.basename("/foo/bar/"), "bar")
+    lu.assertEquals(cosmo.path.basename("file.txt"), "file.txt")
 end
 
-function test_re_module_exists()
-    lu.assertNotNil(re, "re module should be compiled into lua")
+function test_cosmo_path_dirname()
+    lu.assertEquals(cosmo.path.dirname("/foo/bar/baz.txt"), "/foo/bar")
+    lu.assertEquals(cosmo.path.dirname("/foo/bar/"), "/foo")
 end
 
-function test_re_search()
-    lu.assertEquals(re.search([[hello]], "hello world"), "hello")
-    lu.assertNil(re.search([[xyz]], "hello world"))
+function test_cosmo_path_join()
+    lu.assertEquals(cosmo.path.join("foo", "bar"), "foo/bar")
+    lu.assertEquals(cosmo.path.join("/foo", "bar"), "/foo/bar")
+    lu.assertEquals(cosmo.path.join("foo", "/bar"), "/bar")
 end
 
-function test_argon2_module_exists()
-    lu.assertNotNil(argon2, "argon2 module should be compiled into lua")
+function test_cosmo_re_exists()
+    lu.assertNotNil(cosmo.re, "cosmo.re should exist")
 end
 
-function test_lsqlite3_module_exists()
-    lu.assertNotNil(lsqlite3, "lsqlite3 module should be compiled into lua")
+function test_cosmo_re_search()
+    lu.assertEquals(cosmo.re.search([[hello]], "hello world"), "hello")
+    lu.assertNil(cosmo.re.search([[xyz]], "hello world"))
 end
 
-function test_lsqlite3_open_memory()
-    local db = lsqlite3.open_memory()
+function test_cosmo_argon2_exists()
+    lu.assertNotNil(cosmo.argon2, "cosmo.argon2 should exist")
+end
+
+function test_cosmo_sqlite3_exists()
+    lu.assertNotNil(cosmo.sqlite3, "cosmo.sqlite3 should exist")
+end
+
+function test_cosmo_sqlite3_open_memory()
+    local db = cosmo.sqlite3.open_memory()
     lu.assertNotNil(db)
     db:close()
 end
